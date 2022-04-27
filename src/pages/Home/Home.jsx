bb8bfa4b76a54be9f6ec2ed2de080e024233bf6d
mykie88 <mykhelmyers@gmail.com>
2022-04-27 23:55:25 +0100

    created product components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,42 +3,39 @@ import './Home.css';
 export default function Home() {
     return(
         <main>
-            <section className="main-image">
-                <img src="./images/main-image.jpeg" alt="" />
-            </section>
+
+            <img className="main-image" src="./images/homeImage.jpg" alt="" />
 
             <section className="order-now">
-                <h3>
-                    ORDER NOW
-                </h3>
+                <h1> ORDER NOW </h1>
                 <div className="order-now-list">
                     <ul>
                         <li>
-                            <img src="./images/like.png" />
+                            <img src="./images/like.png" alt=''/>
                             <p>Everyday Affordable Value Meal</p>
                         </li>
                         <li>
-                            <img src="./images/fried chicken.png" />
+                            <img src="./images/fried-chicken.png" alt=''/>
                             <p>Soulfully Spiced Fried Chicken</p>
                         </li>
                         <li>
-                            <img src="./images/rotiserrie.png" />
+                            <img src="./images/rotiserrie.png" alt=''/>
                             <p>Delicious Rotesserie Chicken</p>
                         </li>
                         <li>
-                            <img src="./images/grilled-chicken.png" />
+                            <img src="./images/grilled-chicken.png" alt=''/>
                             <p>Flame Grilled Chicken</p>
                         </li>
                         <li>
-                            <img src="./images/Burger.png" />
+                            <img src="./images/Burger.png" alt=''/>
                         <p>Burger, Wraps & Chickwizz</p>
                         </li>
                         <li>
-                            <img src="./images/tasty-sides.png" />
+                            <img src="./images/tasty-sides.png" alt=''/>
                             <p>Tasty Sides</p>
                         </li>
                         <li>
-                            <img src="./images/ice-cream.png" />
+                            <img src="./images/ice-cream.png" alt=''/>
                             <p>Sweet Treats & Beverages</p>
                         </li>
                     </ul>
